cb5b8a9bdd1a136da74f65ac0fb123ec72ce66a9
Andrea Sanz <asanzdj@gmail.com>
2017-04-17 13:33:43 +0200

    Updated content

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( navbar.css )
 Rails.application.config.assets.precompile += %w( content.css )
 Rails.application.config.assets.precompile += %w( buttons.css )
+Rails.application.config.assets.precompile += %w( codeBlocks.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
